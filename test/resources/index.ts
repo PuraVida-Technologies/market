d7ce5f30f71f299e8f55de02229529510f73d94d
Milad E. Fahmy <miladezzat.f@gmail.com>
2022-07-04 20:32:49 +0200

    test: create post resources

--- test/resources/index.ts
+++ test/resources/index.ts
@@ -1,1 +1,1 @@
-export { generateCategory } from './post';
+export { generateCategory } from './category';
--- test/resources/post.ts
+++ test/resources/post.ts
@@ -1,12 +1,39 @@
 import * as faker from 'faker';
 import * as mongoose from 'mongoose';
-import { Category } from '../../src/models';
+import { POST_STATUS } from '../../src/common/constants';
+import { Post } from '../../src/models';
 
-export const generateCategory = () => {
-  const category = new Category();
+export const generatePost = () => {
+  const post = new Post();
 
-  category._id = new mongoose.Types.ObjectId().toString();
-  category.name = faker.datatype.string(10);
+  const images = [];
 
-  return category;
+  for (let index = 0; index < 5; index++) {
+    images.push(faker.image.animals());
+  }
+
+  post._id = new mongoose.Types.ObjectId().toString();
+  post.name = faker.datatype.string(10);
+  post.description = faker.datatype.string(100);
+  post.mainImageUrl = faker.image.avatar();
+  post.imagesUrls = images;
+  post.address = faker.datatype.string(15);
+  post.location = {
+    coordinates: [+faker.address.longitude(), +faker.address.latitude()],
+    type: 'Point',
+  };
+
+  post.ownerInfo = {
+    email: faker.internet.email(),
+    phoneNumber: faker.phone.phoneNumber(),
+    isEmailVerified: false,
+    isPhoneVerified: false,
+  };
+
+  post.userId = new mongoose.Types.ObjectId().toString();
+  post.price = faker.datatype.number();
+  post.status = POST_STATUS.PENDING;
+  post.openHours = faker.datatype.string(20);
+
+  return post;
 };
