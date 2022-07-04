b26b6cf70b0c6a491c2796b75b0968c40b535567
Milad E. Fahmy <miladezzat.f@gmail.com>
2022-07-04 20:29:58 +0200

    feat: register models

--- src/admin/admin-post/admin-post.module.ts
+++ src/admin/admin-post/admin-post.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AdminPostService } from './admin-post.service';
 import { AdminPostResolver } from './admin-post.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Category, CategorySchema, Post, PostSchema } from '../../models';
 
 @Module({
-  providers: [AdminPostResolver, AdminPostService]
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Category.name,
+        schema: CategorySchema,
+      },
+      {
+        name: Post.name,
+        schema: PostSchema,
+      },
+    ]),
+  ],
+  providers: [AdminPostResolver, AdminPostService],
+  exports: [AdminPostService], //this for deps injection
 })
 export class AdminPostModule {}
