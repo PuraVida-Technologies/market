7b2c64e1eb59c7ecf7103da9e4da034a8992e024
Milad E. Fahmy <miladezzat.f@gmail.com>
2022-07-11 13:46:03 +0200

    test: add test cases

--- test/marketplace-user-ratepost.e2e-spec.ts
+++ test/marketplace-user-ratepost.e2e-spec.ts
@@ -7,14 +7,19 @@ import { getModelToken } from '@nestjs/mongoose';
 import { setupTestApp } from './resources/app-test.module';
 import { closeInMongodConnection } from './mongo.connection';
 
-import { Category, Post } from '../src/models';
-import { generateCategory, generatePost } from './resources';
+import { Category, Post, UserRatePost } from '../src/models';
+import {
+  generateCategory,
+  generatePost,
+  generateUserPostRate,
+} from './resources';
 
 describe('Marketplace User Rate Post resolvers (e2e)', () => {
   let app: INestApplication;
   let moduleFixture: TestingModule;
   let postModel: Model<Post>;
   let categoryModel: Model<Category>;
+  let userRatePostModel: Model<UserRatePost>;
 
   beforeAll(async () => {
     ({ app, moduleFixture } = await setupTestApp());
@@ -22,6 +27,11 @@ describe('Marketplace User Rate Post resolvers (e2e)', () => {
     categoryModel = moduleFixture.get<Model<Category>>(
       getModelToken(Category.name),
     );
+
+    userRatePostModel = moduleFixture.get<Model<UserRatePost>>(
+      getModelToken(UserRatePost.name),
+    );
+
     const category = generateCategory();
 
     await categoryModel.create(category);
@@ -137,6 +147,78 @@ describe('Marketplace User Rate Post resolvers (e2e)', () => {
       const { errors } = body;
 
       expect(errors).toBeDefined();
+      expect(errors[0].message).not.toBeNull();
+    });
+  });
+
+  describe('Remove User Rate Post', () => {
+    afterEach(async () => {
+      await postModel.deleteMany({});
+    });
+
+    it('Should delete user rate post successfully', async () => {
+      const category = await categoryModel.findOne({}).lean();
+      const post = await postModel.create({
+        ...generatePost(),
+        category,
+        categoryId: category._id,
+      });
+
+      const userRatePost = await userRatePostModel.create({
+        ...generateUserPostRate(),
+        postId: post._id,
+      });
+
+      const createUserRatePost = `
+      mutation {
+        removeUserRatePost(id: "${userRatePost._id.toString()}"){
+          _id
+          userId
+          value
+          createdAt
+          updatedAt
+        }
+      }`;
+
+      const { body } = await request(app.getHttpServer())
+        .post('/graphql')
+        .set('Content-Type', 'application/json')
+        .send({ query: createUserRatePost });
+      expect(body.errors).toBeUndefined();
+
+      const { _id } = body.data.removeUserRatePost;
+      const deletedUserRatePost = await userRatePostModel.findById(_id);
+      expect(deletedUserRatePost.isDeleted).toBe(true);
+    });
+
+    it('Should fail to delete user rate post, rate not exists', async () => {
+      const category = await categoryModel.findOne({}).lean();
+      const post = await postModel.create({
+        ...generatePost(),
+        category,
+        categoryId: category._id,
+      });
+
+      const removeUserRatePost = `
+      mutation {
+        removeUserRatePost(id: "${post._id.toString()}"}){
+         _id
+         userId
+         value
+         createdAt
+         updatedAt
+        }
+      }`;
+
+      const { body } = await request(app.getHttpServer())
+        .post('/graphql')
+        .set('Content-Type', 'application/json')
+        .send({ query: removeUserRatePost });
+
+      const { errors } = body;
+
+      expect(errors).toBeDefined();
+
       expect(errors[0].message).not.toBeNull();
     });
   });
