f80160c1fdca5339efe387cb0ebdce89436a9d20
Milad E. Fahmy <miladezzat.f@gmail.com>
2022-07-11 13:14:21 +0200

    feat: create resolver

---
+++ src/marketplace/user-rate-post/user-rate-post.resolver.ts
@@ -0,0 +1,22 @@
+import { Resolver, Mutation, Args, Int } from '@nestjs/graphql';
+import { UserRatePostService } from './user-rate-post.service';
+import { UserRatePost } from './entities/user-rate-post.entity';
+import { CreateUserRatePostInput } from './dto/create-user-rate-post.input';
+
+@Resolver(() => UserRatePost)
+export class UserRatePostResolver {
+  constructor(private readonly userRatePostService: UserRatePostService) {}
+
+  @Mutation(() => UserRatePost)
+  createUserRatePost(
+    @Args('createUserRatePostInput')
+    createUserRatePostInput: CreateUserRatePostInput,
+  ) {
+    return this.userRatePostService.create(createUserRatePostInput);
+  }
+
+  @Mutation(() => UserRatePost)
+  removeUserRatePost(@Args('id', { type: () => Int }) id: number) {
+    return this.userRatePostService.remove(id);
+  }
+}
